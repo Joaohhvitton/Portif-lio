e9aa1f119cb175813a3bcf7a6323273fe3869d6e
João Vitor Lima <168872093+Joaohhvitton@users.noreply.github.com>
2026-02-24 11:17:30 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://itgexftkwxgdqcvcoedh.supabase.co",
+  supabaseAnonKey: "sb_publishable_fO8kHRJ31KJcfnwy_MEVrA_Ce5ZYu_y",
+  tableName: "public.base_pix",
+};
