c47ba694e81c59330ea6d8e8bbe8acaefece427c
João Vitor Lima <168872093+Joaohhvitton@users.noreply.github.com>
2026-03-05 14:41:58 -0400

    Update sciprt.js

--- sciprt.js
+++ sciprt.js
@@ -1,3 +1,11 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://itgexftkwxgdqcvcoedh.supabase.co",
+  supabaseAnonKey: "sb_publishable_fO8kHRJ31KJcfnwy_MEVrA_Ce5ZYu_y",
+  tableName: "public.base_pix",
+};
+
+
+
 const STORAGE_KEY = "redeflex.demands.v1";
 
 const config = window.APP_CONFIG || {};
